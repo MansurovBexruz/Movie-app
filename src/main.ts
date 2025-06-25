d9cf48d4bb2ed6898dc893e69ad711d44cfb5c43
Kamolkhan <azadhanovkamolhon@gmail.com>
2025-06-25 18:30:41 +0500

    ohirgi

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,19 @@
 import { movies } from "./type";
 
-// Elementlarni olish
-const searchInput = document.getElementById("searchInput") as HTMLInputElement;
+const searchInput = document.getElementById("search") as HTMLInputElement;
 const searchBtn = document.getElementById("searchBtn") as HTMLButtonElement;
 const results = document.getElementById("results") as HTMLDivElement;
 
-// Qidirish funksiyasi
 searchBtn.addEventListener("click", () => {
   const query = searchInput.value.trim().toLowerCase();
 
-  // Faqat nomi mos keladigan kinolarni olish
   const matchedMovies = movies.filter(movie =>
     movie.title.toLowerCase().includes(query)
   );
 
-  // Ekranni tozalash
   results.innerHTML = "";
 
   if (matchedMovies.length > 0) {
-    // Faqat topilganlarni chiqarish
     matchedMovies.forEach(movie => {
       const div = document.createElement("div");
       div.innerHTML = `
@@ -31,7 +26,6 @@ searchBtn.addEventListener("click", () => {
       results.appendChild(div);
     });
   } else {
-    // Topilmasa
     results.innerHTML = `<p>Bunday kino mavjud emas</p>`;
   }
 });
